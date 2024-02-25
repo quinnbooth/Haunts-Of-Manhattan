ce9d6ba0afdfed32a1988f1f0b1f9ed88a43202e
Quinn Booth <qabooth@gmail.com>
2024-02-25 16:00:21 -0500

    Formatting

--- main/static/view.js
+++ main/static/view.js
@@ -2,8 +2,8 @@ let img = $('<img class="resultImg">').attr('src', result['image']);
 let title = $(`<span id="resultTitle">${result['title']}</span><br>`);
 let opened = (`<span id="resultOpened"> est. ${result['opened']}</span>`);
 let address = $(`<span id="resultAddress">${result['address']}</span><br>`);
-let nearby = $(`<div id="resultNearby">Nearby: </div><br>`);
-let description = $(`<span class="resultDescription">${result['description']}</span>`);
+let nearby = $(`<div id="resultNearby">Near: </div><br>`);
+let description = $(`<span id="resultDescription">${result['description']}</span>`);
 let infoCol = $(`<div class="col-6">`);
 let imgCol = $(`<div class="col-6 imgCol">`);
 let infoRow = $(`<div class="row" id="topRow">`);
@@ -20,6 +20,7 @@ $(document).ready(function() {
     infoRow.append(infoCol).append(imgCol);
 
     $("#container").append(infoRow);
+    $("#container").append("<div class='row' id='comments'><div class='col-12'>Comments:</div></div>")
 
     result['comments'].forEach(function(comment) {
         $("#container").append(`<div class="row commentEntry"><div class="col-12">${comment}</div></div>`);
